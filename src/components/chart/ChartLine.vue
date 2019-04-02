500bab6e2df4799619856b9fc20447bc15cf79fd
felix <805908873@qq.com>
2019-04-02 20:33:02 +0800

    Create ChartLine.vue

---
+++ src/components/chart/ChartLine.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>折线图</h1>
+    <ve-line :data="chartData" :settings="chartSettings"></ve-line>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data: function () {
+      this.chartSettings = {
+        xAxisType: 'time'
+      }
+      return {
+        chartData: {
+          columns: ['time', 'quantity'],
+          rows: []
+        }
+      }
+    },
+    methods: {
+            getYingliList(){
+                this.getRequest("/api/finance/getYingliList").then(resp=> {
+                    if (resp && resp.status == 200) {
+                        var data = resp.data;
+                        this.chartData.rows = resp.data
+                        }
+                })
+            }
+    },
+    created: function(){
+        this.getYingliList()
+    }
+  }
+</script>
